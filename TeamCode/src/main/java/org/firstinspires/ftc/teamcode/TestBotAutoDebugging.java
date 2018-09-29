1c8e2eb2649544089880bfe36325509fa76768a1
mvideet <mvideet@gmail.com>
2018-09-29 13:06:25 -0500

    TesBotAutoDebugging

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestBotAutoDebugging.java
@@ -0,0 +1,249 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@Autonomous(name="Trying with 2 rev hubs", group="TestBot")
+
+public class TestBotAutoDebugging extends OpMode
+{
+    /* Declare OpMode members. */
+    TestBot robot = new TestBot(); // use the class created to define a Testbot's hardware
+    int state = 0;  // used to represent the current state in the state machine
+    int initialPosition = 0;  // used to grab the position of a robot at the beginning of a move
+
+    /*
+     * Code to run ONCE when the driver hits INIT
+     */
+    @Override
+    public void init()
+    {
+
+        /* Initialize the hardware variables.
+         * The init() method of the hardware class does all the work here
+         */
+        robot.init(hardwareMap);
+
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData("Say", "Hello RoboSapiens");    //
+
+        // Set all motors to use RUN_USING_ENCODERS if encoders are installed.
+        robot.leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        //start();
+    }
+
+    /*
+     * Code to run ONCE when the driver hits PLAY
+     */
+    @Override
+    public void start() {
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
+     */
+    @Override
+    public void loop() {
+
+        switch (state)
+        {
+
+
+            // Drive Forward indefinitely
+            case 0:
+                robot.rightDrive.setDirection(DcMotorSimple.Direction.FORWARD);
+                robot.leftDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+                initialPosition = robot.leftDrive.getCurrentPosition();
+                robot.leftDrive.setPower(1);
+                robot.rightDrive.setPower(1);
+                //robot.arm.setPower(1);
+                state++;
+                break;
+            // Stop after set distance
+            case 1:
+                if ( (robot.leftDrive.getCurrentPosition() - initialPosition) > 5000 )
+                {
+                    robot.leftDrive.setPower(0.0);
+                    robot.rightDrive.setPower(0.0);
+                    state++;
+                }
+                break;
+
+
+            // Drive Backward indefinitely
+            case 2:
+                robot.rightDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+                robot.leftDrive.setDirection(DcMotorSimple.Direction.FORWARD);
+                initialPosition = robot.leftDrive.getCurrentPosition();
+                robot.leftDrive.setPower(1);
+                robot.rightDrive.setPower(0);
+                state++;
+                break;
+
+            // Stop After Set Distance
+            case 3:
+                if ( (robot.leftDrive.getCurrentPosition() - initialPosition) > 5000 )
+                {
+                    robot.leftDrive.setPower(0.0);
+                    robot.rightDrive.setPower(0.0);
+                    state++;
+                }
+                break;
+            case 4:
+                robot.rightDrive.setDirection(DcMotorSimple.Direction.FORWARD);
+                robot.leftDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+                initialPosition = robot.leftDrive.getCurrentPosition();
+                robot.leftDrive.setPower(1);
+                robot.rightDrive.setPower(1);
+                state++;
+                break;
+            // Stop after set distance
+            case 5:
+                if ( (robot.leftDrive.getCurrentPosition() - initialPosition) > 5000 )
+                {
+                    robot.leftDrive.setPower(0.0);
+                    robot.rightDrive.setPower(0.0);
+                    state++;
+                }
+                break;
+
+
+            // Drive Backward indefinitely
+            case 6:
+                robot.rightDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+                robot.leftDrive.setDirection(DcMotorSimple.Direction.FORWARD);
+                initialPosition = robot.leftDrive.getCurrentPosition();
+                robot.leftDrive.setPower(1);
+                robot.rightDrive.setPower(0);
+                state++;
+                break;
+
+            // Stop After Set Distance
+            case 7:
+                if ( (robot.leftDrive.getCurrentPosition() - initialPosition) > 5000 )
+                {
+                    robot.leftDrive.setPower(0.0);
+                    robot.rightDrive.setPower(0.0);
+                    state++;
+                }
+                break;
+            case 8:
+                robot.rightDrive.setDirection(DcMotorSimple.Direction.FORWARD);
+                robot.leftDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+                initialPosition = robot.leftDrive.getCurrentPosition();
+                robot.leftDrive.setPower(1);
+                robot.rightDrive.setPower(1);
+                state++;
+                break;
+            // Stop after set distance
+            case 9:
+                if ( (robot.leftDrive.getCurrentPosition() - initialPosition) > 5000 )
+                {
+                    robot.leftDrive.setPower(0.0);
+                    robot.rightDrive.setPower(0.0);
+                    state++;
+                }
+                break;
+
+
+            // Drive Backward indefinitely
+            case 10:
+                robot.rightDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+                robot.leftDrive.setDirection(DcMotorSimple.Direction.FORWARD);
+                initialPosition = robot.leftDrive.getCurrentPosition();
+                robot.leftDrive.setPower(1);
+                robot.rightDrive.setPower(0);
+                state++;
+                break;
+
+            // Stop After Set Distance
+            case 11:
+                if ( (robot.leftDrive.getCurrentPosition() - initialPosition) > 5000 )
+                {
+                    robot.leftDrive.setPower(0.0);
+                    robot.rightDrive.setPower(0.0);
+                    state++;
+                }
+                break;
+            case 12:
+                robot.rightDrive.setDirection(DcMotorSimple.Direction.FORWARD);
+                robot.leftDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+                initialPosition = robot.leftDrive.getCurrentPosition();
+                robot.leftDrive.setPower(1);
+                robot.rightDrive.setPower(1);
+                state++;
+                break;
+            // Stop after set distance
+            case 13:
+                if ( (robot.leftDrive.getCurrentPosition() - initialPosition) > 5000 )
+                {
+                    robot.leftDrive.setPower(0.0);
+                    robot.rightDrive.setPower(0.0);
+                    state++;
+                }
+                break;
+
+
+            // Drive Backward indefinitely
+            case 14:
+                robot.rightDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+                robot.leftDrive.setDirection(DcMotorSimple.Direction.FORWARD);
+                initialPosition = robot.leftDrive.getCurrentPosition();
+                robot.leftDrive.setPower(1);
+                robot.rightDrive.setPower(0);
+                state++;
+                break;
+
+            // Stop After Set Distance
+            case 15:
+                if ( (robot.leftDrive.getCurrentPosition() - initialPosition) > 5000 )
+                {
+                    robot.leftDrive.setPower(0.0);
+                    robot.rightDrive.setPower(0.0);
+                    state++;
+                }
+                break;
+
+//            case 16:
+//                robot.rightclaw.setPosition(0.5);
+//                state++;
+//
+//                break;
+//            case 17:
+//                if ( (robot.rightclaw.GetPosition() - initialPosition) > 100)
+//                {
+//                    robot.rightclaw.setPosition(0);
+//                    state++;
+//                }
+//                break;
+
+
+
+
+
+
+
+
+
+default:
+        break;
+
+        }
+        telemetry.addData("4. Left Front Position: ", robot.leftDrive.getCurrentPosition());
+        //telemetry.addData("4. Arm Front Position: ", robot.arm.getCurrentPosition());
+        }
+
+
+
+/*
+ * Code to run ONCE after the driver hits STOP
+ */
+@Override
+public void stop()
+        {
+
+        }
+        }
